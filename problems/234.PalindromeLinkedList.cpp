1e6c0f595b43e49af20efcd609d54756e5902ef7
bjlhuan <mailtohl@qq.com>
2018-09-29 21:42:45 +0800

    second

--- problems/234.PalindromeLinkedList.cpp
+++ problems/234.PalindromeLinkedList.cpp
@@ -36,15 +36,23 @@
 class Solution {
 public:
 	bool isPalindrome(ListNode* head) {
+		// zero or one
 		if (!head || !head->next)
 			return true;
 
-		int len = getLen(head);
-		int odd = len % 2 == 1;
-		int off = len / 2;
+		ListNode* slow = head, *fast = head->next;
+		while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		// odd
+		const bool odd = (nullptr == fast);
+		slow = slow->next;
+
 
 		ListNode* left = head, *pre = nullptr;
-		while (off-->0)
+		while (left != slow)
 		{
 			auto next = left->next;
 			left->next = pre;
@@ -53,36 +61,27 @@ public:
 		}
 
 		if (odd)
-			left = left->next;
-
-		while (left && pre)
+			left = pre->next;
+		else
+			left = pre;
+		
+		while (left && slow)
 		{
-			if (left->val != pre->val)
+			if (left->val != slow->val)
 				return false;
 
 			left = left->next;
-			pre = pre->next;
+			slow = slow->next;
 		}
 
 		return true;
-
 	}
-	int getLen(ListNode* head)
-	{
-		int len = 0;
-		while (head)
-		{
 
-			head = head->next;
-			++len;
-		}
-		return len;
-	}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	ListNode* head = makeList({ 1, 2 ,2, 1});
+	ListNode* head = makeList({ 1, 2, 5, 2, 1});
 	Solution s;
 	bool b = s.isPalindrome(head);
 	cout << boolalpha << b  << endl;
