3c04f80ab44a780f1b6fa8bb791e0171069f21d9
bjlhuan <mailtohl@qq.com>
2018-10-03 10:07:04 +0800

    find single number by xor

--- 136.singleNUmber.cpp
+++ 136.singleNUmber.cpp
@@ -20,12 +20,9 @@
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
-		sort(nums.begin(), nums.end());
-		for (size_t i = 1; i < nums.size(); i += 2)
-			if (nums[i] != nums[i - 1])
-				return nums[i - 1];
-
-		return nums.back();
-
+		int res = 0;
+		for (auto& v : nums)
+			res ^= v;
+		return res;
 	}
 };
